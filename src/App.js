95499f239c3fa6c4e112d29aafbc2219f11c65ad
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-09-29 20:23:18 +0700

    small App changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+
 import store from './redux/store';
 import Navigator from './navigation';
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <Navigator />
-    </Provider>
-  );
-}
+export default () => (
+  <Provider store={store}>
+    <Navigator />
+  </Provider>
+);
