f6dad67c343e7d314ecdb6a1dfcdbff5f981addb
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-09-29 20:22:56 +0700

    separated services

---
+++ src/services/index.js
@@ -0,0 +1,16 @@
+export const spotifyAuthOptions = {
+  clientID: '7f3b314d392d405dabc16fb93308762a',
+  sessionUserDefaultsKey: 'SpotifySession',
+  redirectURL: 'rnspotify://auth',
+  scopes: [
+    'user-read-private',
+    'playlist-read',
+    'playlist-read-private',
+    'streaming',
+  ],
+};
+
+export const googleAuthOptions = {
+  iosClientId:
+    '839963583954-do66c05dv80tm42e1gpfhgf4pevfvddm.apps.googleusercontent.com',
+};
