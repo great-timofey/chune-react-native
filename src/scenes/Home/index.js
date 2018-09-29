3999646db01e5c405a7fc99f653f44edbfa9757f
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-09-29 10:49:47 +0700

    set react-native-spotify for ios

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import Spotify from 'rn-spotify-sdk';
 import styles from './styles';
 
 type Props = {
   navigation: Object
 }
 
-function Home(props: Props) {
-  const { navigation } = props;
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Counter')}>
-        <Text style={styles.buttonText}>Redux Counter</Text>
-      </TouchableOpacity>
-    </View>
-  );
+class Home extends Component<Props> {
+  constructor() {
+    super();
+
+    this.state = {
+      spotifyInitialized: false,
+    };
+  }
+
+  componentDidMount() {
+    const spotifyOptions = {
+      clientID: '7f3b314d392d405dabc16fb93308762a',
+      sessionUserDefaultsKey: 'SpotifySession',
+      redirectURL: 'rnspotify://auth',
+      scopes: ['user-read-private', 'playlist-read', 'playlist-read-private', 'streaming'],
+    };
+    Spotify.initialize(spotifyOptions).then(_ => Spotify.login().then((loggedIn) => {
+      if (loggedIn) {
+        // logged in
+        // alert('logged');
+        // this.goToPlayer();
+        this.setState({ spotifyInitialized: true });
+      } else {
+        // cancelled
+      }
+    }).catch((error) => {
+      // error
+      alert('Error', error.message);
+    }));
+    // initialize Spotify if it hasn't been initialized yet
+    // if (!Spotify.isInitialized()) {
+    //   // initialize spotify
+    //     // update UI state
+    //     alert('logged');
+    //     // handle initialization
+    //     if (loggedIn) {
+    //       // this.goToPlayer();
+    //     }
+    //   }).catch((error) => {
+    //     Alert.alert('Error', error.message);
+    //   });
+    // }
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { spotifyInitialized } = this.state;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Counter')}>
+          <Text style={styles.buttonText}>{spotifyInitialized ? 'yep' : 'no'}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 export default Home;
