6f66e1c78565d6492ae0b7ad918a5f8eb04612f2
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-01 09:20:46 +0700

    migrated styles to styled-components

--- src/scenes/Auth/index.js
+++ src/scenes/Auth/index.js
@@ -1,10 +1,10 @@
-import Spotify from 'rn-spotify-sdk';
 import React, { PureComponent, Fragment } from 'react';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
+
+import Spotify from 'rn-spotify-sdk';
+import styled from 'styled-components';
 import { LoginManager as FacebookLoginManager } from 'react-native-fbsdk';
 import { GoogleSignin as GoogleLoginManager } from 'react-native-google-signin';
 
-import styles from './styles';
 import images from '../../global/images';
 import colors from '../../global/colors';
 import AuthInput from '../../components/AuthInput';
@@ -40,15 +40,6 @@ export default class Auth extends PureComponent<Props> {
     Spotify.initialize(spotifyAuthOptions);
   }
 
-  _validate = some => {
-    this.setState({ input: some });
-    if (this.state.input.length > 1) {
-      this.setState({ error: { name: 'wrong name' } });
-    } else {
-      this.setState({ error: { name: null } });
-    }
-  };
-
   _handleAuthSpotify = async () => {
     const loggedIn = await Spotify.login();
     if (loggedIn) {
@@ -77,19 +68,19 @@ export default class Auth extends PureComponent<Props> {
 
   _handleAuthFb = () => {
     FacebookLoginManager.logInWithReadPermissions(['public_profile']).then(
-      result => {
+      (result) => {
         if (result.isCancelled) {
           console.log('Login cancelled');
         } else {
           console.log(
-            `Login success with permissions: ${result.grantedPermissions.toString()}`
+            `Login success with permissions: ${result.grantedPermissions}`,
           );
           this.setState({ authorized: true });
         }
       },
-      error => {
+      (error) => {
         console.log(`Login fail with error: ${error}`);
-      }
+      },
     );
   };
 
@@ -107,67 +98,130 @@ export default class Auth extends PureComponent<Props> {
     const { isSignUp } = this.state;
     return (
       <Fragment>
-        <View style={styles.container}>
-          <Image
-            source={images.logo_chune}
-            style={[styles.logo, !isSignUp && { marginBottom: 60 }]}
-          />
-          <Text style={styles.greeting}>
-            {`Sign ${isSignUp ? 'up' : 'in'}`}
-          </Text>
-          <Text style={styles.loginInfo}>by email</Text>
-          <View style={styles.inputContainer}>
-            {isSignUp && (
-              <AuthInput
-                label="Name"
-                validation={this._validate}
-                error={this.state.error.name}
-              />
-            )}
-            <AuthInput label="Email" />
-            <AuthInput label="Password" password />
-          </View>
+        <Container>
+          <Logo source={images.logo_chune} reducedMargin={isSignUp} />
+          <Invitation>{`Sign ${isSignUp ? 'up' : 'in'}`}</Invitation>
+          <InvitationPromptEmail>by email</InvitationPromptEmail>
+          <Form>
+            {isSignUp && <FormField label="Name" />}
+            <FormField label="Email" />
+            <FormField label="Password" password />
+          </Form>
           {!isSignUp && (
-            <TouchableOpacity style={styles.forgotPasswordButton}>
-              <Text style={styles.forgotPasswordText}>Forgot password?</Text>
-            </TouchableOpacity>
+            <ForgetPasswordButton>
+              <ForgetPasswordText>Forgot password?</ForgetPasswordText>
+            </ForgetPasswordButton>
           )}
-          <TouchableOpacity
-            onPress={this._handleEnter}
-            style={styles.enterButton}
-          >
-            <Text style={styles.enterButtonText}>
+          <EnterButton onPress={this._handleEnter}>
+            <EnterButtonText>
               {`Sign ${isSignUp ? 'up' : 'in'}`}
-            </Text>
-          </TouchableOpacity>
-          <Text style={[styles.loginInfo, { marginTop: 25, marginBottom: 15 }]}>
-            or by socials
-          </Text>
-          <View>
+            </EnterButtonText>
+          </EnterButton>
+          <InvitationPromptSocials>or by socials</InvitationPromptSocials>
+          <SocialsContainer>
             <SpotifySignInButton onPress={this._handleAuthSpotify} />
             <AuthSocials
               onFbAuth={this._handleAuthFb}
               onGoogleAuth={this._handleAuthGoogle}
             />
-          </View>
-        </View>
-        <View
-          style={[styles.snackBar, { paddingHorizontal: isSignUp ? 90 : 70 }]}
-        >
-          <Text>
+          </SocialsContainer>
+        </Container>
+        <SnackBar shrinkedText={isSignUp}>
+          <SnackBarText>
             {isSignUp ? 'Have an account?' : "Don't have an account?"}
-          </Text>
-          <TouchableOpacity
-            onPress={() =>
-              this.setState(({ isSignUp }) => ({ isSignUp: !isSignUp }))
+          </SnackBarText>
+          <ToggleEnterModeButton
+            onPress={() => this.setState(({ isSignUp }) => ({ isSignUp: !isSignUp }))
             }
           >
-            <Text style={styles.snackBarButtonText}>
+            <ToggleEnterModeButtonText>
               {`Sign ${isSignUp ? 'in' : 'up'}`}
-            </Text>
-          </TouchableOpacity>
-        </View>
+            </ToggleEnterModeButtonText>
+          </ToggleEnterModeButton>
+        </SnackBar>
       </Fragment>
     );
   }
 }
+
+const EnterButton = styled.TouchableOpacity`
+  justify-content: center;
+  background-color: ${colors.accent};
+  width: 100%;
+  height: 45;
+  border-radius: 3;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  padding-top: 35;
+  padding-horizontal: 16;
+  align-items: center;
+  background-color: white;
+`;
+
+const Logo = styled.Image`
+  margin-bottom: ${props => (props.reducedMargin ? '35' : '60')};
+`;
+
+const Invitation = styled.Text`
+  font-size: 20;
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 30;
+  color: ${colors.black};
+`;
+
+const InvitationPromptEmail = styled.Text`
+  font-size: 14;
+  color: ${colors.greyPrompts};
+`;
+
+const InvitationPromptSocials = styled(InvitationPromptEmail)`
+  margin-top: 25;
+  margin-bottom: 15;
+`;
+
+const Form = styled.View`
+  width: 100%;
+  margin-bottom: 15;
+`;
+
+const ForgetPasswordButton = styled.TouchableOpacity`
+  margin-bottom: 20;
+`;
+
+const ForgetPasswordText = styled.Text`
+  color: ${colors.accent};
+  font-size: 16;
+`;
+
+const EnterButtonText = styled.Text`
+  font-size: 16;
+  text-align: center;
+  color: ${colors.white};
+`;
+
+const SnackBar = styled.View`
+  padding-vertical: 10;
+  padding-horizontal: ${props => (props.shrinkedText ? 90 : 70)};
+  background-color: #f2f2f2;
+  width: 100%;
+  height: 44;
+  flex-direction: row;
+  align-items: baseline;
+  justify-content: space-between;
+  border-top-width: 1;
+  border-top-color: ${colors.grey};
+`;
+
+const ToggleEnterModeButtonText = styled.Text`
+  color: ${colors.accent};
+  font-weight: 700;
+  font-size: 20;
+`;
+
+const SocialsContainer = styled.View``;
+const FormField = styled(AuthInput)``;
+const SnackBarText = styled.Text``;
+const ToggleEnterModeButton = styled.TouchableOpacity``;
