97c25975e6edc991ee5a1c4983290b21a7dea0be
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-01 10:50:07 +0700

    fixed import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import store from './redux/store';
+import store from './redux';
 import Navigator from './navigation';
 
 export default () => (
