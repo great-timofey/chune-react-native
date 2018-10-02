9857d9c683f49e3d877ec331b535c716a3ae9af1
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-02 14:04:05 +0700

    moved tab render logic into separate function

--- src/components/CustomTabView/index.js
+++ src/components/CustomTabView/index.js
@@ -7,39 +7,41 @@ import colors from '../../global/colors';
 import HomeScreen from '../../screens/Home';
 import AuthScreen from '../../screens/auth';
 
-export default () => (
-  <ScrollableTabView
-    renderTabBar={() => (
-      <TabBar
-        scrollContainerStyle={{
-          width: '100%',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-        tabBarStyle={{
-          height: 48,
-          marginTop: 0,
-          paddingTop: 20,
-          backgroundColor: colors.accent,
-          shadowRadius: 3,
-          shadowColor: 'black',
-          shadowOffset: { height: 0, width: 0 },
-          shadowOpacity: 0.75,
-        }}
-        tabBarTextStyle={{
-          color: '#D8D8D8',
-          fontFamily: 'Roboto-Bold',
-        }}
-        activeTabTextStyle={{
-          color: 'white',
-        }}
-        underlineHeight={Platform.OS === 'android' ? 4 : 2}
-        underlineColor="white"
-      />
-    )}
-  >
-    <HomeScreen tabLabel={{ label: 'HOME' }} />
-    <HomeScreen tabLabel={{ label: 'FOR YOU' }} />
-    <HomeScreen tabLabel={{ label: 'ARTISTS & EVENTS' }} />
-  </ScrollableTabView>
-);
+export default () => {
+  const _renderTab = () => (
+    <TabBar
+      scrollContainerStyle={{
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+      tabBarStyle={{
+        height: 48,
+        marginTop: 0,
+        paddingTop: 20,
+        backgroundColor: colors.accent,
+        shadowRadius: 3,
+        shadowColor: 'black',
+        shadowOffset: { height: 0, width: 0 },
+        shadowOpacity: 0.75,
+      }}
+      tabBarTextStyle={{
+        color: '#D8D8D8',
+        fontFamily: 'Roboto-Bold',
+      }}
+      activeTabTextStyle={{
+        color: 'white',
+      }}
+      underlineHeight={Platform.OS === 'android' ? 4 : 2}
+      underlineColor="white"
+    />
+  );
+
+  return (
+    <ScrollableTabView renderTabBar={_renderTab}>
+      <HomeScreen tabLabel={{ label: 'HOME' }} />
+      <HomeScreen tabLabel={{ label: 'FOR YOU' }} />
+      <HomeScreen tabLabel={{ label: 'ARTISTS & EVENTS' }} />
+    </ScrollableTabView>
+  );
+};
