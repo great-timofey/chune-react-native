d2671052ba5077a074cf930492e3845c0fd20a09
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-03 10:04:51 +0700

    configurated Player view for both platforms

--- src/components/player.js
+++ src/components/player.js
@@ -5,37 +5,75 @@ import {
   TouchableOpacity,
   ScrollView,
   Dimensions,
+  Platform,
   Image,
 } from 'react-native';
 import Modal from 'react-native-modal';
 import styled from 'styled-components';
+import ViewOverflow from 'react-native-view-overflow';
 
 import { colors } from '../global';
 
+/*
+        style={{
+          ...Platform.select({
+            ios: {
+              flex: 1,
+              top: 0,
+              left: -20,
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+              position: 'absolute',
+              height: Dimensions.get('window').height * 0.97,
+              width: Dimensions.get('window').width,
+              backgroundColor: 'white',
+            },
+            android: {
+              overflow: 'visible',
+              backgroundColor: 'yellow',
+              width: 1000,
+              height: 1200,
+            },
+          }),
+        }}
+        */
+
 export default ({ isVisible, callback }) => (
-  <Modal
-    swipeThreshold={100}
-    swipeDirection="down"
-    onSwipe={callback}
-    isVisible={isVisible}
-  >
-    <View
+  <ViewOverflow>
+    <Modal
+      swipeThreshold={100}
+      swipeDirection="down"
+      onSwipe={callback}
+      isVisible={isVisible}
       style={{
         flex: 1,
-        top: 0,
-        left: -20,
+        height: '100%',
+        backgroundColor: 'white',
         borderTopLeftRadius: 10,
         borderTopRightRadius: 10,
         position: 'absolute',
-        height: Dimensions.get('window').height * 0.97,
         width: Dimensions.get('window').width,
-        backgroundColor: 'white',
+        ...Platform.select({
+          android: {
+            top: -10,
+            left: -18,
+          },
+          ios: {
+            top: 10,
+            left: -20,
+          },
+        }),
       }}
     >
       <Text>Hello!</Text>
-      <TouchableOpacity onPress={callback}>
+      <TouchableOpacity
+        style={{
+          alignItems: 'flex-end',
+        }}
+        onPress={callback}
+      >
         <Text>hide me</Text>
       </TouchableOpacity>
-    </View>
-  </Modal>
+    </Modal>
+  </ViewOverflow>
 );
