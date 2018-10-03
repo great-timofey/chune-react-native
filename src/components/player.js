97663223e7a953c0c843ac6efc678be8ca56d078
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-03 11:51:43 +0700

    added top bar to Player + initial migration to styled-components

--- src/components/player.js
+++ src/components/player.js
@@ -14,66 +14,94 @@ import ViewOverflow from 'react-native-view-overflow';
 
 import { colors } from '../global';
 
-/*
-        style={{
-          ...Platform.select({
-            ios: {
-              flex: 1,
-              top: 0,
-              left: -20,
-              borderTopLeftRadius: 10,
-              borderTopRightRadius: 10,
-              position: 'absolute',
-              height: Dimensions.get('window').height * 0.97,
-              width: Dimensions.get('window').width,
-              backgroundColor: 'white',
-            },
-            android: {
-              overflow: 'visible',
-              backgroundColor: 'yellow',
-              width: 1000,
-              height: 1200,
-            },
-          }),
-        }}
-        */
+const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
 
-export default ({ isVisible, callback }) => (
-  <ViewOverflow>
-    <Modal
-      swipeThreshold={100}
-      swipeDirection="down"
-      onSwipe={callback}
-      isVisible={isVisible}
-      style={{
-        flex: 1,
-        height: '100%',
-        backgroundColor: 'white',
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-        position: 'absolute',
-        width: Dimensions.get('window').width,
-        ...Platform.select({
-          android: {
-            top: -10,
-            left: -18,
-          },
-          ios: {
-            top: 10,
-            left: -20,
-          },
-        }),
-      }}
-    >
-      <Text>Hello!</Text>
-      <TouchableOpacity
-        style={{
-          alignItems: 'flex-end',
-        }}
-        onPress={callback}
+export default ({ isVisible, callback }) => {
+  const toRender = (
+    <ViewOverflow>
+      <ModalView
+        swipeThreshold={100}
+        swipeDirection="down"
+        onSwipe={callback}
+        isVisible={isVisible}
       >
-        <Text>hide me</Text>
-      </TouchableOpacity>
-    </Modal>
-  </ViewOverflow>
-);
+        <TopPanelContainer>
+          <TopPanelButtonContainer onPress={callback}>
+            <TopPanelButton />
+          </TopPanelButtonContainer>
+        </TopPanelContainer>
+        <ToggleTypeContainer>
+          <ToggleTypeButton>
+            <ToggleTypeButtonText active>Top Tracks</ToggleTypeButtonText>
+          </ToggleTypeButton>
+          <ToggleTypeButton>
+            <ToggleTypeButtonText active={false}>
+              Chune Supply
+            </ToggleTypeButtonText>
+          </ToggleTypeButton>
+        </ToggleTypeContainer>
+      </ModalView>
+    </ViewOverflow>
+  );
+  return toRender;
+};
+
+const ModalView = styled(Modal)`
+  flex: 1;
+  height: 100%;
+  background-color: white;
+  position: absolute;
+  border-top-left-radius: 10;
+  border-top-right-radius: 10;
+  width: ${windowWidth};
+  ${Platform.select({
+    android: {
+      top: -10,
+      left: -18,
+    },
+    ios: {
+      top: 10,
+      left: -20,
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+  })};
+`;
+
+const TopPanelContainer = styled.View`
+  width: ${windowWidth};
+  padding-top: 3;
+  height: 20;
+  align-items: center;
+  border-top-left-radius: 10;
+  border-top-right-radius: 10;
+`;
+
+const TopPanelButtonContainer = styled.TouchableOpacity``;
+
+const TopPanelButton = styled.View`
+  width: 60;
+  height: 3;
+  background-color: #bdbdbd;
+`;
+
+const ToggleTypeContainer = styled.View`
+  flex-direction: row;
+  width: 260;
+  height: 30;
+`;
+
+const ToggleTypeButton = styled.TouchableOpacity`
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+`;
+
+const TextRobotoMedium = styled.Text`
+  font-family: Roboto-Medium;
+  color: ${props => (props.active ? colors.accent : colors.grey)};
+`;
+
+const ToggleTypeButtonText = styled(TextRobotoMedium)`
+  text-transform: uppercase;
+`;
