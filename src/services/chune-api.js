20b96f151d3c5a5becb9d26ea8ffd58e6332448e
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-04 13:05:00 +0700

    separated helpers for image loading

--- src/services/chune-api.js
+++ src/services/chune-api.js
@@ -6,9 +6,14 @@ export const API = axios.create({
     'content-type': 'application/json',
   },
 });
+
 export const setUserToken = (token) => {
   API.defaults.headers.common.Authorization = `JWT ${token}`;
 };
+
 export const clearUserToken = () => {
   API.defaults.headers.common.Authorization = null;
 };
+
+export const homeImagesPrefix = 'http://api-stage.chunesupply.com/static/imgs/thumbs/medium/';
+export const featuredArticleUrl = 'http://api-stage.chunesupply.com/static/imgs/full/';
