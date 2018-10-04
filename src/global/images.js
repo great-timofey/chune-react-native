23957b85d72a0f0f12fb70b518005cd83c2bde2c
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-04 17:22:55 +0700

    added Spotify logo for Swiper

--- src/global/images.js
+++ src/global/images.js
@@ -1,9 +1,10 @@
 const images = {
-  logo_chune: require('./images/logo_chune/logo_chune.png'),
-  logo_spotify: require('./images/logo_spotify/logo_spotify.png'),
-  logo_google_plus: require('./images/logo_google_plus/logo_google_plus.png'),
-  logo_facebook: require('./images/logo_facebook/logo_facebook.png'),
-  logo_twitter: require('./images/logo_twitter/logo_twitter.png'),
+  logoChune: require('./images/logo_chune/logo_chune.png'),
+  logoSpotify: require('./images/logo_spotify/logo_spotify.png'),
+  logoSpotifyColorless: require('./images/logo_spotify_colorless/logo_spotify_colorless.png'),
+  logoGooglePlus: require('./images/logo_google_plus/logo_google_plus.png'),
+  logoFacebook: require('./images/logo_facebook/logo_facebook.png'),
+  logoTwitter: require('./images/logo_twitter/logo_twitter.png'),
 };
 
 export default images;
