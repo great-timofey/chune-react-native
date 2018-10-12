13dacecf64c41da6056e25d4392138cc15831559
Maxim Kolesnikov <mxklsn@gmail.com>
2018-10-12 12:38:05 +0700

    Hotfix

--- src/components/PlayerView.js
+++ src/components/PlayerView.js
@@ -9,7 +9,7 @@ import Icon from 'react-native-vector-icons/Feather';
 import ViewOverflow from 'react-native-view-overflow';
 
 import Control from '~components/Control';
-import PlayerTopPanel from './player-top-panel';
+import PlayerTopPanel from './PlayerTopPanel';
 
 import { API } from '~services/chune-api';
 import { colors, components, utils } from '~global';
