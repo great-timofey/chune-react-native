3e7a4bc3d247c175302134fc6bbde3b611386300
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-17 14:34:13 +0700

    some auth redux code cleanup

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -1,11 +1,13 @@
 import { AUTH_ACTIONS } from './constants';
 
 export const setUser = user => ({
-  type: AUTH_ACTIONS.SET_USER, payload: { user },
+  type: AUTH_ACTIONS.SET_USER,
+  payload: { user },
 });
 
 export const setToken = token => ({
-  type: AUTH_ACTIONS.SET_TOKEN, payload: { token },
+  type: AUTH_ACTIONS.SET_TOKEN,
+  payload: { token },
 });
 
 export const userLogout = () => ({ type: AUTH_ACTIONS.LOGOUT });
