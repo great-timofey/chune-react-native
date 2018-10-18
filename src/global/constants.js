75d2bec747dc809b161043d24eeb90d2e1f29da4
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-18 22:40:14 +0700

    added placeholder image for followed artists with no image

--- src/global/constants.js
+++ src/global/constants.js
@@ -1,6 +1,6 @@
-
 const constants = {
-  NO_IMAGE_ARTIST: 'https://via.placeholder.com/200x200',
+  NO_IMAGE_ARTIST_RECOMMENDED: 'https://via.placeholder.com/200x200',
+  NO_IMAGE_ARTIST_FOLLOWED: 'https://via.placeholder.com/40x40',
 };
 
 export default constants;
