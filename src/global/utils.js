36154eab34b75a48d9952190970b0df5bd8eae8a
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-10-19 10:30:39 +0700

    added placeholder convenient function

--- src/global/utils.js
+++ src/global/utils.js
@@ -2,6 +2,8 @@ import { Dimensions, Platform } from 'react-native';
 
 const { width: deviceWidth, height: deviceHeight } = Dimensions.get('window');
 
+export const getPlaceholder = (size) => `https://via.placeholder.com/${size}x${size}`;
+
 export const platformSelect = (ios, android) => (
   Platform.select({ ios, android })
 );
